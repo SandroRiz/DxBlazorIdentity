63bacc68375f23612f97d1d5e6fe8d5d29dec00c
Sandro Rizzetto <sandro.rizzetto@microgate.it>
2021-06-04 11:16:11 +0200

    role fix

--- DxBlazorIdentity/Areas/Identity/Pages/Account/Manage/CreateRoles.cshtml.cs
+++ DxBlazorIdentity/Areas/Identity/Pages/Account/Manage/CreateRoles.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DxBlazorIdentity.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
@@ -15,12 +16,15 @@ namespace DxBlazorIdentity.Areas.Identity.Pages.Account.Manage
       
         private readonly ILogger<CreateRoles> _logger;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
         public CreateRoles(
             ILogger<CreateRoles> logger,
-            RoleManager<IdentityRole> roleManager)
+            RoleManager<IdentityRole> roleManager,
+            UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         [TempData]
@@ -37,6 +41,9 @@ namespace DxBlazorIdentity.Areas.Identity.Pages.Account.Manage
                 await _roleManager.CreateAsync(new IdentityRole { Name = "superuser" });
                 await _roleManager.CreateAsync(new IdentityRole { Name = "user" });
                 await _roleManager.CreateAsync(new IdentityRole { Name = "guest" });
+           
+            var me = await _userManager.GetUserAsync(HttpContext.User);
+            await _userManager.AddToRoleAsync(me, "admin");
             }
             return Page();
         }
---
+++ DxBlazorIdentity/Data/MenuItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DxBlazorIdentity.Data
+{
+   public  class MenuItem
+    {
+        public string Name { get; set; }
+
+        public string Text { get; set; }
+
+        public string NavigateUrl { get; set; }
+    }
+}
