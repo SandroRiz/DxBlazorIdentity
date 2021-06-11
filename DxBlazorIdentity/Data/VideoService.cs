786a774998f97eba0c82d78c646b43ca8956bba4
Sandro Rizzetto <sandro.rizzetto@microgate.it>
2021-06-11 17:49:18 +0200

    video manager

---
+++ DxBlazorIdentity/Data/VideoService.cs
@@ -0,0 +1,26 @@
+﻿
+using DxBlazorIdentity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DxBlazorIdentity.Services
+{
+    public class VideoService 
+    {
+       
+
+        public List<Video> ListAsync()
+        {
+
+            return new List<Video> { new Video { Title="Foo", Description="Foo Lorem Ipsum", VideoDate=DateTime.Now.Date},
+            new Video { Title="Bar", Description="Bar Lorem Ipsum", VideoDate=DateTime.Now.AddDays(-1).Date},
+            new Video { Title="Baz", Description="Baz Lorem Ipsum", VideoDate=DateTime.Now.AddDays(-2).Date},
+            };
+        }
+
+    }
+}
---
+++ DxBlazorIdentity/Pages/VideoManager.razor.cs
@@ -0,0 +1,74 @@
+﻿using DevExpress.Blazor;
+using DxBlazorIdentity.Models;
+using DxBlazorIdentity.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DxBlazorIdentity.Pages
+{
+    [AllowAnonymous]
+    public class VideoManagerBase : ComponentBase
+    {
+        [Inject] IJSRuntime JsRuntime { get; set; }
+        public List<Video> Videos { get; set; }
+        public DxDataGrid<Video> MyGrid;
+
+        private VideoService VideoService;
+
+        protected override async Task OnInitializedAsync()
+        {
+
+            VideoService = new VideoService();
+            await BindGrid();
+        }
+        private async Task BindGrid(bool refreshGrid = false)
+        {
+            Videos =  VideoService.ListAsync();
+            if (refreshGrid)
+            {
+                await InvokeAsync(StateHasChanged);
+            }
+        }
+
+        public Task OnInitNewRow(Dictionary<string, object> values)
+        {
+            values.Add("VideoDate", DateTime.Now.Date);
+            return Task.CompletedTask;
+        }
+        public async Task OnRowInserting(IDictionary<string, object> newValues)
+        {
+            //await VideoService.AddAsync(UpdateItem<Video>(new Video(), newValues));
+
+            await BindGrid(true);
+        }
+        public async Task OnRowUpdating(Video dataItem, IDictionary<string, object> newValues)
+        {
+            //await VideoService.UpdateAsync(UpdateItem<Video>(dataItem, newValues));
+        }
+        public async Task OnRowDeleting(Video dataItem)
+        {
+            bool isConfirmed = await JsRuntime.InvokeAsync<bool>("confirmDelete");
+
+            if (isConfirmed && dataItem != null)
+            {
+                //await VideoService.DeleteAsync(dataItem);
+                await BindGrid(true);
+            }
+        }
+
+        private T UpdateItem<T>(T item, IDictionary<string, object> newValues) where T : new()
+        {
+            foreach (var field in newValues.Keys)
+            {
+                item.GetType().GetProperty(field).SetValue(item, newValues[field]);
+            }
+            return item;
+        }
+    }
+}
